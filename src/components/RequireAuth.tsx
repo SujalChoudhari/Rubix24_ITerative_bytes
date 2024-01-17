01b0c7ba13f5ccbedd7557cee323e1d8ddf1d4cf
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-01-17 21:07:00 +0530

    toast msg added

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,12 +1,22 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-
+import { useToast } from "@chakra-ui/toast";
 import { usePocket } from "../contexts/PocketContext";
+import { useEffect } from "react";
 
 export const RequireAuth = () => {
     const { user } = usePocket();
     const location = useLocation();
+    const toast = useToast()
 
     if (!user) {
+        useEffect(() => {
+            toast({
+                title: 'User Not Logged In',
+                description: "We cannot log you in. Sign in to continue",
+                status: 'error',
+                isClosable: true,
+            })
+        }, [])
         return (
             <Navigate to={{ pathname: "/signin" }} state={{ location }} replace />
         );
