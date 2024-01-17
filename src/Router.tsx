89fe70839b4e4680b64d84208b616a4eab536957
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2024-01-17 12:17:56 +0530

    added routes

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'; 
+import Homepage from './pages/Homepage';
+import Complaints from './pages/Complaints';
+import Orders from './pages/Orders';
+import Inventory from './pages/Inventory';
+import TrackOrders from './pages/TrackOrders';
+import SignUp from './components/Signup';
+import SignIn from './components/Signin';
+const RoutesConfig = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage/>} />
+      <Route path="/orders" element={<Orders/>} />
+      <Route path="/inventory" element={<Inventory/>} />
+      <Route path="/track" element={<TrackOrders/>} />
+      <Route path="/complaints" element={<Complaints/>} />
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/signin" element={<SignIn/>} />
+    </Routes>
+  );
+};
+
+export default RoutesConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
+import Routes from './Router.tsx';
+import { BrowserRouter as Router } from 'react-router-dom';
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <Router>
+          <Navbar />
+          <Routes />
+        </Router>
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+}
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+    <Router>
+          <Nav />
+          <Routes />
+        </Router>
+    </ChakraProvider>
+  </React.StrictMode>
